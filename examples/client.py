0f4eec41c47fc2abd6c5478fe3e24871c4cea942
kirisakiken <bezmican96@gmail.com>
2023-01-09 03:36:10 +0300

    feat: add base client side implementation

---
+++ examples/client.py
@@ -0,0 +1,33 @@
+import threading
+import socket
+
+HOST = 'localhost'
+PORT = 12345
+
+def listen_for_messages(sock):
+    while True:
+        data = sock.recv(1024).decode()
+        if not data:
+            break
+        print(data)
+
+def send_message(sock):
+    while True:
+        message = input()
+        if message == "EOF":
+            return
+        sock.send(message.encode())
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((HOST, PORT))
+
+listen_thread = threading.Thread(target=listen_for_messages, args=(sock,))
+send_thread = threading.Thread(target=send_message, args=(sock,))
+
+listen_thread.start()
+send_thread.start()
+
+listen_thread.join()
+send_thread.join()
+
+sock.close()
